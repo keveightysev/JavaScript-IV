892de4f9c2568ba2fccddffff687399e73a35b55
Kevin Smith <keveightysev@gmail.com>
2019-01-24 12:51:25 -0600

    Built Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attrs) {
+        this.name = attrs.name;
+        this.location = attrs.location;
+        this.age = attrs.age;
+        this.gender = attrs.gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
